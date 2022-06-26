1a778637886c5f3c392cb94725be2db512bf6c0b
AZKZero <khanabdullahzowad@gmail.com>
2022-06-26 16:10:31 +0600

    details

--- src/op-list/op-list.controller.ts
+++ src/op-list/op-list.controller.ts
@@ -11,6 +11,72 @@ export class OpListController {
     private opModel: Model<OperatorDocument>,
   ) {}
 
+  private readonly operators = [
+    r6operators.sledge,
+    r6operators.thatcher,
+    r6operators.ash,
+    r6operators.thermite,
+    r6operators.twitch,
+    r6operators.montagne,
+    r6operators.glaz,
+    r6operators.fuze,
+    r6operators.blitz,
+    r6operators.iq,
+    r6operators.buck,
+    r6operators.blackbeard,
+    r6operators.capitao,
+    r6operators.hibana,
+    r6operators.jackal,
+    r6operators.ying,
+    r6operators.zofia,
+    r6operators.dokkaebi,
+    r6operators.lion,
+    r6operators.finka,
+    r6operators.maverick,
+    r6operators.nomad,
+    r6operators.gridlock,
+    r6operators.nokk,
+    r6operators.amaru,
+    r6operators.kali,
+    r6operators.iana,
+    r6operators.ace,
+    r6operators.zero,
+    r6operators.flores,
+    r6operators.osa,
+    r6operators.smoke,
+    r6operators.mute,
+    r6operators.castle,
+    r6operators.pulse,
+    r6operators.doc,
+    r6operators.rook,
+    r6operators.kapkan,
+    r6operators.tachanka,
+    r6operators.jager,
+    r6operators.bandit,
+    r6operators.frost,
+    r6operators.valkyrie,
+    r6operators.caveira,
+    r6operators.echo,
+    r6operators.mira,
+    r6operators.lesion,
+    r6operators.ela,
+    r6operators.vigil,
+    r6operators.alibi,
+    r6operators.maestro,
+    r6operators.clash,
+    r6operators.kaid,
+    r6operators.mozzie,
+    r6operators.warden,
+    r6operators.goyo,
+    r6operators.wamai,
+    r6operators.oryx,
+    r6operators.melusi,
+    r6operators.aruni,
+    r6operators.thunderbird,
+    r6operators.thorn,
+    r6operators.azami,
+  ];
+
   private readonly attackers = [
     r6operators.sledge,
     r6operators.thatcher,
@@ -82,14 +148,26 @@ export class OpListController {
   @Get('list')
   async getOperators(@Query('attacker', ParseBoolPipe) attacker: boolean) {
     return {
-      data: (attacker ? this.attackers : this.defenders).map((value) => ({
-        id: value.id,
-        name: value.name,
-        bio: value.bio,
-        role: value.role,
-        unit: value.unit,
-        png: `https://r6operators.marcopixel.eu/icons/png/${value.id}.png`,
-      })),
+      data: this.operators
+        .filter((value) =>
+          attacker ? value.role == 'Attacker' : value.role == 'Defender',
+        )
+        .map((value) => ({
+          id: value.id,
+          name: value.name,
+          bio: value.bio,
+          role: value.role,
+          unit: value.unit,
+          png: `https://r6operators.marcopixel.eu/icons/png/${value.id}.png`,
+        })),
+    };
+  }
+
+  @Get('operator_det')
+  async getOperatorDetails(@Query('op_id') operatorId: string) {
+    return {
+      ...this.operators.find((value) => value.id === operatorId),
+      png: `https://r6operators.marcopixel.eu/icons/png/${operatorId}.png`,
     };
   }
 }
